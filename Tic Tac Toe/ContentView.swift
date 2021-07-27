986f87bc66434d2554301e2fe9b68bdcca9f8b2b
rahn <ryanahn2000@gmail.com>
2021-07-27 10:14:45 -0700

    Determined if game is tie and finished game

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -68,6 +68,10 @@ struct ContentView: View {
         checkLine(a: 2, b: 5, c: 8)
         checkLine(a: 0, b: 4, c: 8)
         checkLine(a: 2, b: 4, c: 6)
+        if !(gameOver || moves.contains("")) {
+            winMessage = "Cat's Game"
+            gameOver = true
+        }
     }
     
     private func checkLine(a: Int, b: Int, c: Int) {
